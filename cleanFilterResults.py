669850307e13e8916cbe6bde9cd2c061cbbe51c9
m4ggie <maggiewu0807@gmail.com>
2025-09-14 12:55:35 -0400

    added more regex

--- cleanFilterResults.py
+++ cleanFilterResults.py
@@ -23,6 +23,7 @@ df = pd.read_csv("uoft_career_fair_employers.csv")
 def clean_level_of_study(value):
     value = re.sub(r'Graduate Full-time', 'Graduate', str(value), flags=re.IGNORECASE)
     value = re.sub(r'Recent Graduate', 'Recent Graduates', str(value), flags=re.IGNORECASE)
+    value = re.sub(r'Recent Graduatess', 'Recent Graduates', str(value), flags=re.IGNORECASE)
     return value
 
 def clean_hiring_for(value):
@@ -35,14 +36,22 @@ def clean_hiring_for(value):
 def clean_target_programs(value):
     value = re.sub(r'All programs', 'All Programs', str(value), flags=re.IGNORECASE)
     value = re.sub(r'Arts and Sciences', 'Arts & Science', str(value), flags=re.IGNORECASE)
+    value = re.sub(r'Arts & Sciences', 'Arts & Science', str(value), flags=re.IGNORECASE)
     value = re.sub(r'Engineering and Technology', 'Engineering & Technology', str(value), flags=re.IGNORECASE)
+    value = re.sub(r'Engineering & Tech', 'Engineering & Technology', str(value), flags=re.IGNORECASE)
     return value
 
+def clean_opporunities(value):
+    value = re.sub(r'Analysts', 'Analyst', str(value), flags=re.IGNORECASE)
+    value = re.sub(r'Developers', 'Developer', str(value), flags=re.IGNORECASE)
+
+    return value
 
 
 df["Level of Study"] = df["Level of Study"].apply(clean_level_of_study)
 df["Hiring For"] = df["Hiring For"].apply(clean_hiring_for)
 df["Target Programs"] = df["Target Programs"].apply(clean_target_programs)
+df['Opportunities'] = df['Opportunities'].apply(clean_opporunities)
 
 df.to_csv("uoft_career_fair_employers_cleaned.csv", index=False)
 print("done")
